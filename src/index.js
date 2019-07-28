9019e0b3db884ac2f2747b15ad77c329ebf20f8f
Mirek Simek <miroslav.simek@vscht.cz>
2019-07-28 15:23:33 +0200

    index for input field

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@
  *
  */
 import TaxonomyEditor from './components/TaxonomyEditor.vue'
+import DialogTaxonomyInput from './components/DialogTaxonomyInput.vue'
 
 const VueTaxonomy = {
     install (Vue) {
         Vue.component('taxonomy-editor', TaxonomyEditor)
+        Vue.component('dialog-taxonomy-input', DialogTaxonomyInput)
     }
 }
 
 export {
     VueTaxonomy,
-    TaxonomyEditor
+    TaxonomyEditor,
+    DialogTaxonomyInput
 }
